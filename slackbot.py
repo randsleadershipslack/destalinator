d37f8ef03f96398d6c7aa59bf9c7969e578f61eb
Roy Rapoport <rrapoport@netflix.com>
2015-12-28 00:29:30 -0800

    initial working version of slackbot integration code

---
+++ slackbot.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python2.7
+
+import requests
+
+import util
+
+
+class Slackbot(object):
+
+    def __init__(self, slack_name, slackbot_token=None, slackbot_token_file=None):
+        self.slack_name = slack_name
+        self.sb_token = util.get_token(slackbot_token, slackbot_token_file)
+        self.url = self.sb_url()
+
+    def sb_url(self):
+        url = "https://{}.slack.com/".format(self.slack_name)
+        url += "services/hooks/slackbot"
+        return url
+
+    def say(self, channel, statement):
+        """
+        channel should not be preceded with '#'
+        """
+        assert channel  # not blank
+        if channel[0] == '#':
+            channel = channel[1:]
+        nurl = self.url + "?token={}&channel=%23{}".format(self.sb_token, channel)
+        p = requests.post(nurl, statement)
+        return p.status_code
