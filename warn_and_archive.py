08465a597123f376176b04d9a20445be9d9fe4b5
Roy Rapoport <rrapoport@netflix.com>
2015-12-28 10:53:13 -0800

    attempt to safe_archive all channels stale for 60 days or longer

--- warn_and_archive.py
+++ warn_and_archive.py
@@ -6,3 +6,4 @@ import destalinator
 sb = slackbot.Slackbot("rands-leadership", slackbot_token_file="sb_token.txt")
 ds = destalinator.Destalinator("rands-leadership", slackbot=sb, api_token_file="api_token.txt")
 ds.warn_all(30)
+ds.safe_archive_all(60)
