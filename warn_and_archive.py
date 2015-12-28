fa05311cb31174b939be41bdbcfeb9013a1beb23
Roy Rapoport <rrapoport@netflix.com>
2015-12-28 10:31:11 -0800

    fix error where we were referring to Destalinator, rather than destalinator.Destalinator

--- warn_and_archive.py
+++ warn_and_archive.py
@@ -4,5 +4,5 @@ import slackbot
 import destalinator
 
 sb = slackbot.Slackbot("rands-leadership", slackbot_token_file="sb_token.txt")
-ds = Destalinator("rands-leadership", slackbot=sb, api_token_file="api_token.txt")
+ds = destalinator.Destalinator("rands-leadership", slackbot=sb, api_token_file="api_token.txt")
 ds.warn_all(30)
