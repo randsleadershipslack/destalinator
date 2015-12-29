ef90b48eac4c77bb4d87bb2708948d88b1270863
David Lovitch <dlovitch@gmail.com>
2015-12-28 20:05:44 -0800

    Adding Heroku scheduling

---
+++ Procfile
@@ -0,0 +1,1 @@
+clock: python scheduler.py
---
+++ scheduler.py
@@ -0,0 +1,19 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+import slackbot
+import destalinator
+import os
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job("cron", hour=17)
+def destalinate_daily():
+    print("Destalinating")
+    if not os.environ["SLACK_SLACKBOT_TOKEN"] or api_token = os.environ["SLACK_API_TOKEN"]:
+        print "ERR: Missing at least one Slack environment variable."
+    else:
+        sb = slackbot.Slackbot("rands-leadership", slackbot_token=os.environ["SLACK_SLACKBOT_TOKEN"])
+        ds = Destalinator("rands-leadership", slackbot=sb, api_token=os.environ["SLACK_API_TOKEN"])
+        ds.warn_all(30)
+        print("OK: destalinated")
+
+sched.start()
