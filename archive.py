c44cc26c5b82dee37476245f6d83582c81a296c7
Roy Rapoport <rrapoport@netflix.com>
2015-12-29 01:57:04 -0800

    only archive, don't warn

--- archive.py
+++ archive.py
@@ -3,7 +3,11 @@
 import slackbot
 import destalinator
 
-sb = slackbot.Slackbot("rands-leadership", slackbot_token_file="sb_token.txt")
-ds = destalinator.Destalinator("rands-leadership", slackbot=sb, api_token_file="api_token.txt")
-ds.warn_all(30)
-ds.safe_archive_all(60)
+def archive():
+
+    sb = slackbot.Slackbot("rands-leadership", slackbot_token_file="sb_token.txt")
+    ds = destalinator.Destalinator("rands-leadership", slackbot=sb, api_token_file="api_token.txt")
+    ds.safe_archive_all(60)
+
+if __name__ == "__main__":
+    archive()
