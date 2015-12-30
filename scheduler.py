9c774da08566f61ee407319984e38ba5a48ffa60
David Lovitch <dlovitch@gmail.com>
2015-12-30 01:49:03 -0800

    Updated scheduler

--- scheduler.py
+++ scheduler.py
@@ -1,19 +1,25 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-import slackbot
-import destalinator
+from warner import Warner
+from archiver import Archiver
 import os
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job("cron", minute=30)
-def destalinate_daily():
+@sched.scheduled_job("cron", minute=0)
+def destalinate_job():
     print("Destalinating")
-    if not os.environ["SLACK_SLACKBOT_TOKEN"] or not os.environ["SLACK_API_TOKEN"]:
+    if "SB_TOKEN" not in os.environ or "API_TOKEN" not in os.environ:
         print "ERR: Missing at least one Slack environment variable."
+	#print "1 {}".format(os.environ["SLACK_SLACKBOT_TOKEN"])
+	#print "2 {}".format(os.environ["SLACK_API_TOKEN"])
     else:
-        sb = slackbot.Slackbot("rands-leadership", slackbot_token=os.environ["SLACK_SLACKBOT_TOKEN"])
-        ds = Destalinator("rands-leadership", slackbot=sb, api_token=os.environ["SLACK_API_TOKEN"])
-        ds.warn_all(30)
+        warner = Warner()
+        archiver = Archiver()
+        print("Warning")
+        warner.warn()
+        print("Archiving")
+        archiver.archive()
         print("OK: destalinated")
+    print("END: destalinate_job")
 
 sched.start()
