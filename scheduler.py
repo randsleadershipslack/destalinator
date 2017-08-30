522e5e04b2a75a1c4c863116d8ada8c04e122c1a
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2017-08-30 10:35:24 -0500

    Use logging directly in main

--- scheduler.py
+++ scheduler.py
@@ -12,8 +12,6 @@ import flagger
 
 raven_client = RavenClient()
 
-logger = logging.getLogger(__name__)
-
 # When testing changes, set the "TEST_SCHEDULE" envvar to run more often
 if os.getenv("TEST_SCHEDULE"):
     schedule_kwargs = {"hour": "*", "minute": "*/10"}
@@ -25,21 +23,21 @@ sched = BlockingScheduler()
 
 @sched.scheduled_job("cron", **schedule_kwargs)
 def destalinate_job():
-    logger.info("Destalinating")
+    logging.info("Destalinating")
     if "SB_TOKEN" not in os.environ or "API_TOKEN" not in os.environ:
-        logger.error("Missing at least one Slack environment variable.")
+        logging.error("Missing at least one Slack environment variable.")
     else:
         try:
             warner.Warner().warn()
             archiver.Archiver().archive()
             announcer.Announcer().announce()
             flagger.Flagger().flag()
-            logger.info("OK: destalinated")
+            logging.info("OK: destalinated")
         except Exception as e:  # pylint: disable=W0703
             raven_client.captureException()
             if not os.getenv('SENTRY_DSN'):
                 raise e
-    logger.info("END: destalinate_job")
+    logging.info("END: destalinate_job")
 
 
 if __name__ == "__main__":
