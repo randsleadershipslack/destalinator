12b9269b272b71d6c3079b8ed28ebaa9b24cab73
Roy Rapoport <github@ols.inorganic.org>
2022-04-03 15:04:58 -0700

    do nthing but the announcer() stuff

--- scheduler.py
+++ scheduler.py
@@ -3,10 +3,7 @@ import logging
 from apscheduler.schedulers.blocking import BlockingScheduler
 from raven.base import Client as RavenClient
 
-import warner
-import archiver
 import announcer
-import flagger
 from config import get_config
 
 
@@ -37,10 +34,7 @@ def destalinate_job():
         )
     else:
         try:
-            archiver.Archiver().archive()
-            warner.Warner().warn()
             announcer.Announcer().announce()
-            flagger.Flagger().flag()
             logging.info("OK: destalinated")
         except Exception as e:  # pylint: disable=W0703
             raven_client.captureException()
