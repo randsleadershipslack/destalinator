8376dd781118681bfa30f6cdf1e6d7906d5c3abb
Roy Rapoport <github@ols.inorganic.org>
2022-04-03 19:28:42 -0700

    change invocation to drop users_list

--- tests/test_announcer.py
+++ tests/test_announcer.py
@@ -11,7 +11,7 @@ import tests.mocks as mocks
 
 class AnnouncerAnnounceTest(unittest.TestCase):
     def setUp(self):
-        slacker_obj = mocks.mocked_slacker_object(channels_list=fixtures.channels, users_list=fixtures.users)
+        slacker_obj = mocks.mocked_slacker_object(channels_list=fixtures.channels)
         self.slackbot = mocks.mocked_slackbot_object()
         with mock.patch.dict(os.environ, {'DESTALINATOR_ACTIVATED': 'true'}):
             self.announcer = announcer.Announcer(slacker_injected=slacker_obj, slackbot_injected=self.slackbot)
