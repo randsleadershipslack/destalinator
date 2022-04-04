f8aa6a59c8c712eff2ccd26fd3117123140b1b5e
Roy Rapoport <github@ols.inorganic.org>
2022-04-03 19:38:26 -0700

    more vestige removal

--- tests/test_announcer.py
+++ tests/test_announcer.py
@@ -13,10 +13,3 @@ class AnnouncerAnnounceTest(unittest.TestCase):
         self.slackbot = mocks.mocked_slackbot_object()
         with mock.patch.dict(os.environ, {'DESTALINATOR_ACTIVATED': 'true'}):
             self.announcer = announcer.Announcer(slacker_injected=slacker_obj, slackbot_injected=self.slackbot)
-
-    def test_announce_posts_to_announce_channel(self):
-        def channel_message_test(channel):
-            """Ensure that an announce message contains the name of the channel being announced."""
-            return lambda message: channel['name'] in message
-
-        self.announcer.announce()
